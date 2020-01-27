54074a69824685e44c0b47a40da402b0bc19900a
hencarvalho <57905858+hencarvalho@users.noreply.github.com>
2020-01-27 17:08:56 +0000

    more changes

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Definição estátitica, fornece as páginas HTML tal como estão na pasta www
 app.use(express.static('www'));
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:1234@cluster0-nbwfa.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 // Inicio do express à escuta na porta 4000
 app.listen(4000, function () {
     console.log("Server running at http://localhost:4000");});
